d3ec165b22aee7c2a44f60301484785e75aae1a1
Alisha Kelkar <akelkar@email.unc.edu>
2024-02-25 09:34:42 -0500

    aboutus added

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -7,23 +7,23 @@ const Card = () => {
         <h1 className='main'> ABOUT US</h1>
                <div className="row">
                     <div className="leftCard">
-                        <h1>12 years</h1>
+                        <h2 className="number">12 years</h2>
                         <h3 className="insideText">The average age at which kids get their first phone</h3>
                      </div>
     
 
                 <div className="rightCard">
-                  <h1>7 hours</h1>
+                <h2 className="number"> 7 hours</h2>
                   <h3 className="insideText">The average amount of time Americans spend on their screens per day.</h3>
                 </div>
               </div>
               <div className="heading">
-                <h1>The average amount of time spent</h1>
+                <h1 className='heading'>The average amount of time spent</h1>
             </div>
               <div className="row">
                 <div className="subCard">
-                  <h2>0.26 hours</h2>
-                  <h4> reading </h4>
+                  <h2 className="font">0.26 hours</h2>
+                  <h4 className="font"> reading </h4>
                 </div>
                 <div className="subCard">
                   <h2>0.29 hours</h2>
@@ -34,8 +34,8 @@ const Card = () => {
                   <h4> socializing </h4>
                 </div>
               </div>
-              <div className="heading">
-                <h1> We spend so much time online it's about time you go outside and</h1>
+              <div className="text">
+                <h1 className="text"> We spend so much time online it's about time you go outside and</h1>
                 </div>
             <div className="main">
                 <h1> TOUCH THE GRASS</h1>
